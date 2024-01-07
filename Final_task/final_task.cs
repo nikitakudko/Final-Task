b32796e0a92fd03cd278adb8996002a810e68945
nikitakudko <nikitakudko@gmail.com>
2024-01-07 19:21:58 +0300

    add two metods in final_task

--- Final_task/final_task.cs
+++ Final_task/final_task.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] CreateArrayByUser(int size)
+{
+    string[] arr = new string [size];
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine("Введите элемент массива");
+        arr[i]= Console.ReadLine();
+    }
+    return arr;
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]:F2}, ");
+        else Console.Write($"{array[i]:F2}");
+    }
+    Console.Write("]");
+}
