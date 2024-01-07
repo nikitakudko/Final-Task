67cbafcaf7799d4d4a8ed54a8aad7082c9cabd19
nikitakudko <nikitakudko@gmail.com>
2024-01-07 19:26:57 +0300

    Find solution

--- Final_task/final_task.cs
+++ Final_task/final_task.cs
@@ -19,3 +19,38 @@ void PrintArray(string[] array)
     }
     Console.Write("]");
 }
+
+int CountOfElem(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if (array[i].Length <= 3) count++;
+    }
+    return count; 
+}
+
+string [] ArrayWhithElemLessThanOrEqualThree (string[] array, int size)
+{
+    string[] newArr = new string[size];
+    int j = 0;
+    int i = 0;
+    while(i < array.Length)
+    {
+    if (array[i].Length <= 3) 
+        {
+            newArr[j] = array[i];
+            j++;
+        }
+    i++; 
+    }
+    return newArr;
+}
+
+Console.WriteLine("Введите размер массива");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+string[] arr = CreateArrayByUser(sizeArray);
+PrintArray(arr);
+int count = CountOfElem(arr);
+Console.WriteLine();
+PrintArray(ArrayWhithElemLessThanOrEqualThree(arr, count));
